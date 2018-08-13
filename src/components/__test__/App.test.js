6edce9386367518cbef6ce0e0507d449f2ef5b2d
Isaac Pierce <contact@isaacdpierce.com>
2018-08-12 19:20:22 -0700

    Create CommentBox Component and testing

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+// Helper method provided by Jest to setup common test logic
+// Will run before EACH and every test
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 // 'it' refers to the App component
 it('shows a comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+// check for comment list
+it('shows a comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
