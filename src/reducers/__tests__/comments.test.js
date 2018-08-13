05483f4dbf3d6498362939cc20a2d1499e047bf2
Isaac Pierce <contact@isaacdpierce.com>
2018-08-13 15:40:01 -0700

    Add comment action has correct type test

--- src/reducers/__tests__/comments.test.js
+++ src/reducers/__tests__/comments.test.js
@@ -10,3 +10,9 @@ it('handles actions of type SAVE_COMMENT', () => {
 
   expect(newState).toEqual(['New Comment']);
 });
+
+it('handles action with unknown type', () => {
+  const newState = commentsReducer([], { type: 'no type' });
+
+  expect(newState).toEqual([]);
+});
