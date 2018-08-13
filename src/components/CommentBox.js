1bf1f2722d91382f29bec8ddfdd87073212a8e28
Isaac Pierce <contact@isaacdpierce.com>
2018-08-12 23:55:25 -0700

    Setup SAVE_COMMENTS action and reducer. Refactor Provider into Root.js helper function

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
 class CommentBox extends Component {
   state = { comment: '' };
@@ -11,8 +13,8 @@ class CommentBox extends Component {
     e.preventDefault();
 
     // Call an action creator
-
     // Save the comment
+    this.props.saveComment(this.state.comment);
 
     // Clear out the comment box
     this.setState({ comment: '' });
@@ -31,4 +33,7 @@ class CommentBox extends Component {
   }
 }
 
-export default CommentBox;
+export default connect(
+  null,
+  actions
+)(CommentBox);
